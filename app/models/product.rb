9f430a0a75e1c5060439184580badf53b6430aff
Armi Piamonte <armi.piamonte@gmail.com>
2023-08-29 17:50:35 +0200

    [UPDATE]-product and shop

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :shop
+  belongs_to :shop, optional: true
 
   include PgSearch::Model
 
-  pg_search_scope :search_by_name_and_category,
+  pg_search_scope :global_search,
     against: [ :name, :category ],
     using: {
       tsearch: { prefix: true }
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,10 @@ User.create!(email: 'toto@gmail.com', password: 'qwertyuiop')
 
 Shop.create!(location: '82 Cr Balguerie Stuttenberg')
 
-Product.create!(name: 'apricot', category: 'fruits', stock_quantity: 5, price: 3.49)
-Product.create!(name: 'nutella', category: 'snacks', stock_quantity: 10, price: 5.99)
-Product.create!(name: 'kinder bueno', category: 'snacks', stock_quantity: 15, price: 7.01)
+shop = Shop.find(1)
+
+Product.create!(name: 'apricot', category: 'fruits', stock_quantity: 5, price: 3.49, shop: shop )
+Product.create!(name: 'nutella', category: 'snacks', stock_quantity: 10, price: 5.99, shop: shop)
+Product.create!(name: 'kinder bueno', category: 'snacks', stock_quantity: 15, price: 7.01, shop: shop)
 
 p 'Done'
