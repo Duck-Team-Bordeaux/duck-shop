bf0037909bd2fcaa60f0910633d9633cd586385b
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-08-30 15:45:44 +0200

    add product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,21 @@
 class ProductsController < ApplicationController
+  before_action :find_product_of_category, only: :index
+  before_action :set_product, only: :show
 
   def index
-
+    # raise
   end
 
   def show
   end
 
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def find_product_of_category
+    @products = Product.where(category: 'snacks')
+  end
 end
