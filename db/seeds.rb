103d8541d5f22f3f67b5c094532fc25b66494d1b
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-08-31 12:04:00 +0200

    remise comme ce matin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,126 +1,378 @@
-p 'Deleting DB'
+p 'Cleaning DB'
 User.destroy_all
 p 'User => cleaned'
 Shop.destroy_all
 p 'Shop => cleaned'
 Product.destroy_all
-p 'Deleted'
+p 'Product => cleaned'
+p 'DB successfully cleaned'
 
+# -------------USER-------------------------------------------------------------
 
-# -------------USER AND SHOP----------------------------------------------------
-
-p 'creating user and shop'
+p 'creating user'
 User.create!(email: 'toto@gmail.com', password: 'qwertyuiop')
+
+# -------------SHOP-------------------------------------------------------------
+
+p 'creating shop'
 shop = Shop.create!(location: '82 Cr Balguerie Stuttenberg')
+Shop.create!(location: '15 Cr Portal')
+Shop.create!(location: 'Place du Parlement')
+Shop.create!(location: '36 Rue de la Faïencerie')
+Shop.create!(location: "36 Allées d'Orléans")
 
 
 
-# -------------PRODUIT LAITIERS-------------------------------------------------
+# -------------PRODUCT----------------------------------------------------------
+iditetaion = 1
+shops = Shop.all
 
-category = 'Produit Laitiers, Oeufs, Fromages'
-p "Creating #{category}"
-Product.create!(
-  name: 'Beurre tendre doux 250g',
-  description: 'Beurre tendre doux, 82% de matière grasse',
-  category: category,
-  stock_quantity: 10,
-  price: 2.23,
-  shop_id: shop.id
-)
+shops.each do |shop|
 
-Product.create!(
-  name: 'Crème fraiche épaisse légère 15%MG 50cl',
-  description: 'Crème fraîche légère épaisse à 15% de matière grasse',
-  category: category,
-  stock_quantity: 10,
-  price: 1.58,
-  shop_id: shop.id
-)
+  # -------------Snacks-------------------------------------------------
 
-Product.create!(
-  name: 'Oeufs de poules élevées en plein air sans ogm 18 œufs',
-  description: '18 œufs de catégorie A de poules élevées en plein air (code commençant par 1 sur l\'œuf). Calibre Moyen ou gros',
-  category: category,
-  stock_quantity: 10,
-  price: 4.76,
-  image_url: '',
-  shop_id: shop.id
-)
+  category = 'Snacks'
+  p "Creating => #{category}"
 
-Product.create!(
-  name: 'Camembert 250g',
-  description: 'Camembert au lait pasteurisé.',
-  category: category,
-  stock_quantity: 10,
-  price: 1.58,
-  image_url: '',
-  shop_id: shop.id
-)
+  Product.create!(
+    name: 'Sandwich Daunat XXL',
+    description: 'Pour les plus gourmands et les grands appétits, 3 fois plus de plaisir avec ses 3 sandwichs avec une recette classique du marché, le jambon cheddar. Variez les plaisirs avec cette recette au pain suédois fondant.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 2.99,
+    shop_id: iditetaion
+  )
 
-Product.create!(
-  name: 'Emmental français en bloc 400g',
-  description: 'Emmental français Fromage à pâte pressée cuite, au lait pasteurisé',
-  category: category,
-  stock_quantity: 10,
-  price: 4.33,
-  image_url: '',
-  shop_id: shop.id
-)
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '390g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion
+  )
 
-p "#{category} Done!"
+  Product.create!(
+    name: 'DAUNAT Wrap jambon cheddar Daunat',
+    description: 'Be Wrappy: des ingrédients travaillés avec du jambon, du cheddar relevé par une sauce burger, le tout dans une galette de blé, pour varier votre pause plaisir.',
+    unit: '190g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 4.12,
+    shop_id: iditetaion
+  )
 
-# -------------FRUITS, LÉGUMES -------------------------------------------------
+  Product.create!(
+    name: 'Taboulé au poulet rôti Bonduelle',
+    description: 'Taboulé à la viande de cuisse de poulet traitée en salaison rôtie / volaille Française Découvrez le Taboulé Poulet Bonduelle 300g, une semoule fine origine France, sans conservateur et encore plus de plaisir avec du poulet gourmand origine France !',
+    unit: '300g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion
+  )
 
-category = 'Fruits, Légumes'
-p "Creating #{category}"
-Product.create!(
-  name: 'Pommes bicolores',
-  description: 'La pomme Gala a une robe bicolore avec une dominante de rouge, sa saveur douce est à la fois sucrée et acide. C\'est une variété ferme, croquante et juteuse, elle est idéale à croquer pour se désaltérer mais elle peut aussi se consommer en préparation culinaire car elle reste ferme même après cuisson',
-  category: category,
-  stock_quantity: 10,
-  price: 3.99,
-  image_url: '',
-  shop_id: shop.id
-)
+  Product.create!(
+    name: 'Mini bâtonnet de saucisson sec AUVERNOU',
+    description: 'Viande de porc (Origine : France), LACTOSE, sel, poivres, sucre, dextrose, ail, macis, conservateurs : nitrate de potassium et nitrite de sodium, girofle, ferments. Enveloppe (boyau collagénique). 178 g de viande de porc utilisée pour 100g de produit fini.Présence éventuelle de fruits à coque',
+    unit: '30g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 2.99,
+    shop_id: iditetaion
+  )
 
-Product.create!(
-  name: 'Kiwis bio 6 pièces',
-  description: 'Le kiwi est un fruit d\'hiver, avec sa chair verte piquetée de point noirs, le kiwi est plein de bienfaits : outre la vitamine C qui permet de booster le système immunitaire, le kiwi procure des antioxydants et des fibres qui sont bénéfiques au renouvellement cellulaire et au système digestif.',
-  category: category,
-  stock_quantity: 10,
-  price: 4.99,
-  image_url: '',
-  shop_id: shop.id
-)
+  p "#{category} Done!"
 
-Product.create!(
-  name: 'Pastèque bio 1 pièce',
-  description: 'Pastèque bio',
-  category: category,
-  stock_quantity: 10,
-  price: 4.99,
-  image_url: '',
-  shop_id: shop.id
-)
+  # -------------Epicerie -------------------------------------------------
 
-Product.create!(
-  name: 'Tomates rondes en grappes bio 500g',
-  description: 'Les tomates en grappe sont fermes, croquantes et légèrement acidulées. C\'est l\'idéal pour les tartes, les soupes, les sauces, en salade, en plat cuisiné ou encore au grill. La tomate est prête à être dégustée lorsqu\'elle est ferme et souple sous la pression du doigt avec une peau lisse, brillante et sans tache.',
-  category: category,
-  stock_quantity: 10,
-  price: 2.99,
-  image_url: '',
-  shop_id: shop.id
-)
+  category = 'Epicerie'
+  p "Creating => #{category}"
 
-Product.create!(
-  name: 'Carottes Primeur 1kg',
-  description: 'La carotte est un légume plein de bienfaits : riche en bêta-carotènes la carotte donne bonne mine, ses vitamines A permettent d\'améliorer la vue et de protéger la peau, et c\'est un légume très peu calorique, à consommer sans modération',
-  category: category,
-  stock_quantity: 10,
-  price: 4.33,
-  image_url: '',
-  shop_id: shop.id
-)
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
 
-p "#{category} Done!"
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  p "#{category} => Done!"
+
+  # -------------Produit frais -------------------------------------------------
+
+  category = 'Produit frais'
+  p "Creating => #{category}"
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  p "#{category} => Done!"
+
+  # -------------Boisson -------------------------------------------------
+
+  category = 'Boisson'
+  p "Creating => #{category}"
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  p "#{category} => Done!"
+
+  # -------------Hygiène & Beauté -------------------------------------------------
+
+  category = 'Hygiène & Beauté'
+  p "Creating => #{category}"
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  p "#{category} => Done!"
+
+  # -------------Produits d'entretien -------------------------------------------------
+
+  category = 'Epicerie'
+  p "Creating => #{category}"
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  Product.create!(
+    name: 'Sandwich mega baguette Sodebo',
+    description: 'Un sandwich à la générosité débordante pour les Méga faims ! Sandwich composé de 51% de baguette viennoise garni de 14% de mayonnaise allégée en matières grasses, 12% de poulet rôti traité en salaison et 7% d’oeuf.',
+    unit: '230g',
+    category: category,
+    stock_quantity: (0..15).to_a.sample,
+    price: 3.20,
+    shop_id: iditetaion,
+  )
+
+  p "#{category} => Done!"
+
+  iditetaion +=1
+  # --------- fin du Shop.each do |shop|
+end
+# ------------------------ END OF SEED------------------------------------------
+p 'seed success'
