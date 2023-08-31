b07644cb32c6fbdfbad381353d812c38dda8b26b
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-08-31 07:43:44 +0200

    [UPDATE] gabriel-leblond change price column type -> integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_223506) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_053527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_223506) do
     t.string "description"
     t.string "category"
     t.integer "stock_quantity"
-    t.integer "price"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shop_id"
