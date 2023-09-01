3f71413cfb39f8bff73d341bf8f27c8b523824d4
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-09-01 11:19:31 +0200

    add front end feaures

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,6 @@ class ProductsController < ApplicationController
   def index
     @shop = Shop.find(params[:shop_id])
     @products = @shop.products.where(category: params[:category])
-    # @product = Product.find(params[:id])
-    # @shop = Shop.find(params[:shop_id])
-    # raise
   end
 
   def show
