9b327d4785f2f2495e7bc9234ab91c1e70679f3e
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-09-04 16:30:26 +0200

    edit cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,20 @@
 class CartsController < ApplicationController
   def index
-    @user = User.last
+    # @user = User.last
+    @user = current_user
     @carts = Cart.where(user_id: @user.id).reverse
   end
 
   def new
-    @user = User.last
+    # @user = User.last
+    @user = current_user
     @cart = Cart.new
     @shop = Shop.find(params[:shop_id])
   end
 
   def create
     @user = User.last
+    # @user = current_user
     @cart = Cart.new(user_id: @user.id, shop_id: params[:shop_id], progress: 0)
     if @cart.save!
       redirect_to edit_shop_cart_path(id: @cart.id)
@@ -21,6 +24,13 @@ class CartsController < ApplicationController
   end
 
   def edit
+    @user = User.last
+    # @user = current_user
+    # @cart = Cart.new
+    @cart = Cart.last
+    @shop = Shop.find(params[:shop_id])
+    @items = Item.where(cart: @user.cart_ids)
+    raise
   end
 
   def update
