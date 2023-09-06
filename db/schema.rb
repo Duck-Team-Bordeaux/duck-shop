1dd25cc920c71247e999c915ff2eeceac029f761
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-09-06 15:51:29 +0200

    ADD Migration add column duckoins to user and update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_094837) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_134459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_094837) do
     t.float "latitude"
     t.integer "role"
     t.bigint "favorite_shop_id"
+    t.float "duckcoins"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["favorite_shop_id"], name: "index_users_on_favorite_shop_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
