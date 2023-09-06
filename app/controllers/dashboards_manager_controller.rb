a5c796857f3f68a403b2f9185ea33008b5e67487
Natheo Brault <nath.brault.pro@gmail.com>
2023-09-06 15:59:24 +0200

    [ADD] - View wallet

--- app/controllers/dashboards_manager_controller.rb
+++ app/controllers/dashboards_manager_controller.rb
@@ -2,8 +2,16 @@ class DashboardsManagerController < ApplicationController
   def users() end
   def products
     @neg_products = Product.where(stock_quantity: 0, shop: 1)
-    @best_products = Product.where(stock_quantity: 2, shop: 1)
-    @worst_products = Product.where(stock_quantity: 14, shop: 1)
+    @best_products = Product.where('stock_quantity <= 3 AND shop_id = 1')
+    @worst_products = Product.where('stock_quantity >= 13 AND shop_id = 1')
   end
-  def wallet() end
+  def wallet
+    @carts_count = Cart.all.count
+    result = 0
+    Cart.all.each do |cart|
+      result += cart.items.map { |item| item.quantity * item.product.price }.sum.round(2)
+    end
+    @turnover = result.round(2)
+    @moy_cart = (result / Cart.all.count).round(2)
+  end
 end
