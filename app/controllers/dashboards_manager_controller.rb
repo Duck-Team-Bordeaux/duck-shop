ee1e8444ebb187c584560f1364670124d4ca6937
Lilia Bruffaerts <bruffaert@icloud.com>
2023-09-07 14:37:27 +0200

    [Æ∆∆]graph il faut le convertir pour quil corresponde a notre truc etle faire joli

--- app/controllers/dashboards_manager_controller.rb
+++ app/controllers/dashboards_manager_controller.rb
@@ -17,5 +17,25 @@ class DashboardsManagerController < ApplicationController
     end
     @turnover = result.round(2)
     @moy_cart = (result / Cart.all.count).round(2)
+
+    @chart_data = {
+      labels: %w[January February March April May June July],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'transparent',
+        borderColor: '#3B82F6',
+        data: [37, 83, 78, 54, 12, 5, 99]
+      }]
+    }
+
+    @chart_options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
   end
 end
