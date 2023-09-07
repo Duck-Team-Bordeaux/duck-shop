5cb328adf8669b0677e1c40e49efb5c67fe82b7d
Natheo Brault <nath.brault.pro@gmail.com>
2023-09-07 16:35:13 +0200

    [ADD] - navbarmanager

--- app/controllers/dashboards_manager_controller.rb
+++ app/controllers/dashboards_manager_controller.rb
@@ -1,14 +1,20 @@
 class DashboardsManagerController < ApplicationController
   def users
+    @hide_navbar = true
+    @navbarmanager = true
     @users = User.all.reverse
   end
 
   def products
+    @hide_navbar = true
+    @navbarmanager = true
     @neg_products = Product.where(stock_quantity: 0, shop: 1)
     @best_products = Product.where('stock_quantity <= 3 AND shop_id = 1')
   end
 
   def wallet
+    @hide_navbar = true
+    @navbarmanager = true
     @carts_count = Cart.all.count
     result = 0
     Cart.all.each do |cart|
